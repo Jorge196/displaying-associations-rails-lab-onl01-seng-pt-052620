11da4cbab40966196cd420f6d3d92a89e22b62bd
Jorge Ramirez <joramirez96@gmail.com>
2020-10-17 19:52:26 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,10 +2,10 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
-    self.artists.name
+    self.artist.name
   end
 
   def artist_and_title
     artist_name + " - " + self.title
-  end 
+  end
 end
